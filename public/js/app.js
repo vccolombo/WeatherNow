25ed4bf57cdd131d677c8a4d29db02b9ade89624
vccolombo <victorcora98@gmail.com>
2019-10-24 14:07:12 -0300

    integrate frontend with backend forecast

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
-console.log('Client side javascript file is loaded')
+const weather_form = document.querySelector('form');
+const search_input = document.querySelector('input');
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
+
+weather_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    message1.textContent = 'Loading forecast...';
+    message2.textContent = '';
+
+    const search = search_input.value;
+
+    fetch(`http://localhost:3000/weather?search=${search}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return message1.textContent = data.error;
+            }
+
+            message1.textContent = data.location;
+            message2.textContent = data.forecast;
+        });
+    })
+})
