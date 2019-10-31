736ae7d125fd39648c35e3a73c84750d552f700f
Pabolo Pires <pabolo18@gmail.com>
2019-10-31 18:27:27 -0300

    Add icons

---
+++ src/weather_icons.js
@@ -0,0 +1,14 @@
+const weather_icons = {
+    "clear-day": "clear-day.svg",
+    "clear-night": "clear-night.svg",
+    "rain": "rain.svg",
+    "snow": "snow.svg",
+    "sleet": "sleet.svg",
+    "wind": "wind.svg",
+    "fog": "fog.svg",
+    "cloudy": "cloudy.svg",
+    "partly-cloudy-day": "partly-cloudy-day.svg",
+    "partly-cloudy-night": "partly-cloudy-night.svg" 
+};
+
+module.exports = weather_icons;
